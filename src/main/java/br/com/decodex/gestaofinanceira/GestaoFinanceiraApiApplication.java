808101740483d7d10b94c1bb98f4737e72eb3143
TIAGO SILVA RUFINIANO <78699713+TiagoTSR@users.noreply.github.com>
2026-02-01 14:21:10 -0300

    chore: initialize base project structure

---
+++ src/main/java/br/com/decodex/gestaofinanceira/GestaoFinanceiraApiApplication.java
@@ -0,0 +1,13 @@
+package br.com.decodex.gestaofinanceira;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GestaoFinanceiraApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GestaoFinanceiraApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/decodex/gestaofinanceira/GestaoFinanceiraApiApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.decodex.gestaofinanceira;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GestaoFinanceiraApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
