87f92195e28c87f75b4901bb49ea167f027107e3
TIAGO SILVA RUFINIANO <78699713+TiagoTSR@users.noreply.github.com>
2026-02-16 08:51:43 -0300

    chore(security): configure CORS for frontend integration

--- src/main/java/br/com/decodex/gestaofinanceira/controller/CategoriaController.java
+++ src/main/java/br/com/decodex/gestaofinanceira/controller/CategoriaController.java
@@ -6,6 +6,7 @@ import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -25,6 +26,7 @@ import jakarta.validation.Valid;
 
 @RestController
 @RequestMapping("/api/categorias")
+@CrossOrigin("http://localhost:4200")
 public class CategoriaController {
 
 	private final CategoriaService categoriaService;
