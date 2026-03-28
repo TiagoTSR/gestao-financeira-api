8ee405840b8b511ded8d06b1f55ea021cc79fcf0
TIAGO SILVA RUFINIANO <78699713+TiagoTSR@users.noreply.github.com>
2026-03-28 08:01:53 -0300

    feat: add remove and toReplace methods to S3 storage service

--- src/main/java/br/com/decodex/gestaofinanceira/storage/S3.java
+++ src/main/java/br/com/decodex/gestaofinanceira/storage/S3.java
@@ -8,11 +8,13 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
+import org.springframework.util.StringUtils;
 import org.springframework.web.multipart.MultipartFile;
 
 import br.com.decodex.gestaofinanceira.config.property.GestaoApiProperty;
 import software.amazon.awssdk.core.sync.RequestBody;
 import software.amazon.awssdk.services.s3.S3Client;
+import software.amazon.awssdk.services.s3.model.DeleteObjectRequest;
 import software.amazon.awssdk.services.s3.model.PutObjectRequest;
 import software.amazon.awssdk.services.s3.model.PutObjectTaggingRequest;
 import software.amazon.awssdk.services.s3.model.Tagging;
@@ -50,20 +52,43 @@ public class S3 {
             throw new RuntimeException("Problemas ao tentar enviar o arquivo para o S3.", e);
         }
     }
+    
+    public void remove(String objeto) {
+        if (objeto == null) {
+            return;
+        }
+        DeleteObjectRequest deleteRequest = DeleteObjectRequest.builder()
+            .bucket(property.getS3().getBucket())
+            .key(objeto)
+            .build();
+        s3Client.deleteObject(deleteRequest);
+    }
+
+
+    public void toReplace(String objetoAntigo, String objetoNovo) {
+        if (StringUtils.hasText(objetoAntigo)) {
+            this.remove(objetoAntigo);
+        }
+        if (StringUtils.hasText(objetoNovo)) {
+            this.create(objetoNovo);
+        }
+    }
 
     public String configurarUrl(String objeto) {
         return "https://" + property.getS3().getBucket() + ".s3.amazonaws.com/" + objeto;
     }
     
     public void create(String objeto) {
-    	PutObjectTaggingRequest putObjectTaggingRequest = PutObjectTaggingRequest.builder()
+        if (objeto == null) {
+            return;
+        }
+        PutObjectTaggingRequest putObjectTaggingRequest = PutObjectTaggingRequest.builder()
                 .bucket(property.getS3().getBucket())
                 .key(objeto)
                 .tagging(Tagging.builder().tagSet(Collections.emptyList()).build())
                 .build();
-
         s3Client.putObjectTagging(putObjectTaggingRequest);
-	}
+    }
 
     private String gerarNomeUnico(String originalFilename) {
         return UUID.randomUUID().toString() + "_" + originalFilename;
